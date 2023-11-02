fee0ece5e28ece7e097e2afcfbe18b7551e154f5
Anetoni <aho.anttoni@gmail.com>
2023-11-02 13:30:31 +0200

    style and functionality updated

--- library/src/components/LoginForm.jsx
+++ library/src/components/LoginForm.jsx
@@ -1,28 +1,36 @@
 import loginImg from '../assets/login.jpg'
 
-const LoginForm = () => {
+const LoginForm = ({ changeForm }) => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+    <div className='grid grid-cols-1 grid-rows-1 sm:grid-cols-2 h-screen w-full'>
       <div className='hidden sm:block'>
-        <img className="w-full h-full object-cover overflow-hidden" src={loginImg} alt="" />
+        <img className="w-full h-full object-cover" src={loginImg} alt="" />
       </div>
 
       <div className="bg-red-200 flex flex-col justify-center">
-        <form className="max-w-[400px] w-full mx-auto bg-white p-4 rounded">
+        <form className="fixed top-60 right-60 max-w-[400px] w-full mx-auto bg-white p-4 rounded-3xl shadow-2xl">
           <h2 className='text-4xl font-bold text-center py-5'>Log in to access your library</h2>
           <div className="flex flex-col py-2">
             <label>Email</label>
-            <input className="border p-2 rounded" name="email" id="email" placeholder='Your email'></input>
+            <input className="border p-2 rounded-md" type ="email" name="email" id="email" placeholder='Your email'></input>
           </div>
           <div className="flex flex-col py-2">
             <label>Password</label>
-            <input className="border p-2 rounded" type="password" name="password" id="password" placeholder=""></input>
+            <input className="border p-2 rounded-md" type="password" name="password" id="password" placeholder="Your password"></input>
           </div>
-          <button>Sign in</button>
+          <div>
+            <button className="text-sky-600 hover:text-violet-500 hover:underline">Forgot password?</button>
+          </div>
           <div>
             <p>
               <input type="checkbox" /> Remember Me
             </p>
+          </div>
+          <div>
+            <button className="border w-full my-5 py-2 bg-sky-600 hover:bg-violet-500 text-white">Sign in</button>
+          </div>
+          <div>
+            <button onClick={changeForm} className="text-sky-600 hover:text-violet-500 hover:underline center">No account? Register now</button>
           </div>
         </form>
       </div>
--- library/src/components/RegisterForm.jsx
+++ library/src/components/RegisterForm.jsx
@@ -1,18 +1,31 @@
-const RegisterForm = () => {
+import loginImg from '../assets/login.jpg'
+
+const RegisterForm = ({changeForm}) => {
   return (
-    <div>
-      <h1>Register user</h1>
-      <form>
-        <div>
-          E-mail Address: 
-          <input type="text" name="email"></input>
-        </div>
-        <div>
-          Password:
-          <input type="text" name="password"></input>
-        </div>
-        <button type="submit" name="login">login</button>
-      </form>
+    <div className='grid grid-cols-1 grid-rows-1 sm:grid-cols-2 h-screen w-full'>
+      <div className='hidden sm:block'>
+        <img className="w-full h-full object-cover" src={loginImg} alt="" />
+      </div>
+
+      <div className="bg-red-200 flex flex-col justify-center">
+        <form className="fixed top-60 right-60 max-w-[400px] w-full mx-auto bg-white p-4 rounded-3xl shadow-2xl">
+          <h2 className='text-4xl font-bold text-center py-5'>Register your account</h2>
+          <div className="flex flex-col py-2">
+            <label>Email</label>
+            <input className="border p-2 rounded-md" type ="email" name="email" id="email" placeholder='Your email'></input>
+          </div>
+          <div className="flex flex-col py-2">
+            <label>Password</label>
+            <input className="border p-2 rounded-md" type="password" name="password" id="password" placeholder="Your password"></input>
+          </div>
+          <div>
+            <button className="border w-full my-5 py-2 bg-sky-600 hover:bg-violet-500 text-white">Register</button>
+          </div>
+          <div>
+            <button onClick={changeForm} className="text-sky-600 hover:text-violet-500 hover:underline center">Already have an account? Log in</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
